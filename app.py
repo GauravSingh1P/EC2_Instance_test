3ad584c2d63b7f6d399adb4c8509a6d58a90ac64
Gaurav Singh CM <gauravsinghchandramohan@gmail.com>
2026-04-07 23:14:28 +0530

    Added GET/POST testing

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    user_input = None
+    if request.method == 'POST':
+        # This catches the data sent from the HTML form
+        user_input = request.form.get('food_item')
     
-    return render_template('index.html')
+    return render_template('index.html', user_input=user_input)
 
 if __name__ == "__main__":
-    
     app.run(host='0.0.0.0', port=5000)
